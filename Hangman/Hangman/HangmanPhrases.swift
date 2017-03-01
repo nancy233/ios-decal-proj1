4237e8c92a513371710405e230177872f4d4df9f
Nancyyyyyy <jnnancy233@gmail.com>
2017-02-28 20:43:47 -0800

    commit

--- Hangman/Hangman/HangmanPhrases.swift
+++ Hangman/Hangman/HangmanPhrases.swift
@@ -11,6 +11,11 @@ import Foundation
 class HangmanPhrases {
     
     var phrases : NSArray!
+    var status: String = "_"
+    var guessed: String = "Incorrect Guesses: "
+    var correct: Bool = true
+    var youWon: Bool = false
+    var youLost: Bool = false
     
     // Initialize HangmanPhrase with an array of all possible phrases of the Hangman game
     init() {
@@ -22,6 +27,40 @@ class HangmanPhrases {
     func getRandomPhrase() -> String {
         let index = Int(arc4random_uniform(UInt32(phrases.count)))
         return phrases.object(at: index) as! String
+    }
+    
+    func update(buttonTitle:String, phrase:String){
+        let old:String = status
+        if (phrase.contains(buttonTitle)){
+            correct = true
+            for index in 0...phrase.characters.count-1{
+                if (phrase.substring(atIndex: index)==buttonTitle){
+                    if (index==0){
+                        status = buttonTitle
+                    }else{status += buttonTitle}
+                }else if (old.substring(atIndex:index)=="_"){
+                    if (index==0){
+                        status = "_"
+                    }else{status += "_"}
+                }else{
+                    if (index==0){
+                        status = old.substring(atIndex:index)
+                    }else{status += old.substring(atIndex:index)}
+                }
+            }
+        }else{
+            correct = false
+            guessed += buttonTitle
+        }
+    }
+    
+    func Won(phrase: String) -> Bool{
+        for index in 0...phrase.characters.count{
+            if (phrase.substring(atIndex: index) != status.substring(atIndex: index)){
+                return false
+            }
+        }
+        return true
     }
     
 }
--- Hangman/Hangman/HangmanViewController.swift
+++ Hangman/Hangman/HangmanViewController.swift
@@ -9,13 +9,27 @@
 import UIKit
 
 class HangmanViewController: UIViewController {
-
+    var hangmanPhrases = HangmanPhrases()
+    
+    var copyPhrase:String?
+    
+    @IBOutlet var letterButtons: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let hangmanPhrases = HangmanPhrases()
+        
         // Generate a random phrase for the user to guess
+        hangmanPhrases = HangmanPhrases()
         let phrase: String = hangmanPhrases.getRandomPhrase()
+        copyPhrase = phrase
+        for index in 1 ... phrase.characters.count-1{
+            if (phrase.substring(atIndex:index)==" "){
+                hangmanPhrases.status += " "
+            }else{
+                hangmanPhrases.status += "_"
+            }
+        }
+        phraseStatus.text = hangmanPhrases.status
         print(phrase)
     }
 
@@ -34,5 +48,66 @@ class HangmanViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBOutlet weak var hangmanImage: UIImageView!
+    @IBOutlet weak var phraseStatus: UILabel!
+    @IBOutlet weak var incorrect: UILabel!
+    
+    @IBAction func guess(_ sender: UIButton) {
+        if (hangmanPhrases.youWon || hangmanPhrases.youLost){
+        }else{
+            hangmanPhrases.update(buttonTitle: sender.currentTitle!, phrase: copyPhrase!)
+            phraseStatus.text = hangmanPhrases.status
+            incorrect.text = hangmanPhrases.guessed
+            sender.setTitleColor(UIColor.red, for: .normal)
+            sender.isEnabled = false
+        
+        
+        
+            // wrong guess?
+            if (!hangmanPhrases.correct){
+                if (hangmanImage.image == UIImage(named: "hangman1")){
+                    hangmanImage.image = UIImage(named: "hangman2")
+                }else if(hangmanImage.image == UIImage(named: "hangman2")){
+                    hangmanImage.image = UIImage(named: "hangman3")
+                }else if(hangmanImage.image == UIImage(named: "hangman3")){
+                    hangmanImage.image = UIImage(named: "hangman4")
+                }else if(hangmanImage.image == UIImage(named: "hangman4")){
+                    hangmanImage.image = UIImage(named: "hangman5")
+                }else if(hangmanImage.image == UIImage(named: "hangman5")){
+                    hangmanImage.image = UIImage(named: "hangman6")
+                }else if(hangmanImage.image == UIImage(named: "hangman6")){
+                    hangmanImage.image = UIImage(named: "hangman7")
+                
+                    hangmanPhrases.youLost = true
+                    // alert Lose
+                    let alert = UIAlertController(title: "Lost", message: "Sorry, you lost.", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                
+                }
+            }else{
+                // won the game
+                if (hangmanPhrases.Won(phrase: copyPhrase!)){
+                    hangmanPhrases.youWon = true
+                    // alert Won
+                    let alert = UIAlertController(title: "Won", message: "Yay! You won!", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                
+                }
+            }
+        }
+    }
+    
+    @IBAction func restart(_ sender: UIButton) {
+        hangmanPhrases = HangmanPhrases()
+        viewDidLoad()
+        for button in letterButtons as [UIButton]{
+            button.setTitleColor(UIColor.blue, for: .normal)
+            button.isEnabled = true
+        }
+        hangmanImage.image = UIImage(named: "hangman1")
+        
+    }
 
 }
